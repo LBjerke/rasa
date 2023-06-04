8757d3e5b9bd00de4ec5e3239b29d42c565e9312
Your Name <you@example.com>
2023-06-04 14:53:38 -0500

    jupd

--- flake.nix
+++ flake.nix
@@ -90,12 +90,6 @@
         #
         # If you are using a very recent Stackage resolver and an old Nixpkgs,
         # it is almost always necessary to override `all-cabal-hashes`.
-        all-cabal-hashes = final.fetchFromGitHub {
-          owner = "commercialhaskell";
-          repo = "all-cabal-hashes";
-          rev = "9ab160f48cb535719783bc43c0fbf33e6d52fa99";
-          sha256 = "sha256-Hz/xaCoxe4cJBH3h/KIfjzsrEyD915YEVEK8HFR7nO4=";
-        };
       };
 
       # One of our local packages.
